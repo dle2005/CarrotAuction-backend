bed603e99e6fb65e989279ee471538f8ff57d64c
DongKon <dle0129@gmail.com>
2021-05-11 15:07:30 +0900

    Fixed Bug

--- src/main/java/carrotauction/com/carrotauction/repository/UserRepository.java
+++ src/main/java/carrotauction/com/carrotauction/repository/UserRepository.java
@@ -8,5 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUser_id(String user_id);
+    default User findByUser_id(String user_id) {
+        return null;
+    }
 }
